1d041789605b3ea2ebe30fbc96b8ba292fe96556
Heitor Ribeiro <heitor.manhaes@gmail.com>
2024-08-23 11:45:20 -0300

    Adding ReactJS To the Portfolio

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Olá, meu nome é Heitor e Este é meu portifolio!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
